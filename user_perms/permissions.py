4ce21d0afc2da7065941426b1a9c52a45452665c
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-09-07 22:31:27 +0430

    :bug: Fix permission problem for comments

--- user_perms/permissions.py
+++ user_perms/permissions.py
@@ -10,3 +10,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             return True
         elif req.user.is_authenticated:
             return (req.user.is_staff or req.user.is_admin)
+    def has_object_permission(self, req, view,obj):
+        if req.method in permissions.SAFE_METHODS:
+            return True
+        elif req.user.is_authenticated:
+            return (req.user.is_staff or req.user.is_admin)
