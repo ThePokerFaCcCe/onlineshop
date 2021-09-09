4502bf3eb1491595a54c590308d65182d86ae418
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-09-09 17:32:06 +0430

    :hammer: Change CustomerSerializer name & remove email

--- customers/serializers.py
+++ customers/serializers.py
@@ -2,14 +2,13 @@ from rest_framework import serializers
 from .models import Customer
 
 
-class CustomerSerializer(serializers.ModelSerializer):
+class CustomerReadOnlySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Customer
         fields = [
             'pk',
             'username',
-            'email',
             'is_active',
             'is_staff',
             'is_admin'
