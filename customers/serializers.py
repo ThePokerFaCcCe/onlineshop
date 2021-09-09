9b524712e9a9bea66f02cdcdbf93f2e199470ce2
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-09-10 03:19:34 +0430

    :hammer: Create custom serializers for djoser

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from djoser.serializers import UserSerializer as DjoserUserSerializer, UserCreatePasswordRetypeSerializer as DjoserUserCreateSerializer
 from .models import Customer
 
 
@@ -14,3 +15,65 @@ class CustomerReadOnlySerializer(serializers.ModelSerializer):
             'is_admin'
         ]
         read_only_fields = fields
+
+
+class CustomerSerializer(DjoserUserSerializer):
+    class Meta:
+        model = DjoserUserSerializer.Meta.model
+        fields = [
+            'pk',
+            'username',
+            # 'email',
+            # 'password',
+            'first_name',
+            'last_name',
+            'phone_number',
+            'age',
+            'is_active',
+            'is_staff',
+            'is_admin',
+        ]
+
+        extra_kwargs = {
+            # 'password': {'write_only': True},
+            # 'email': {'write_only': True},
+            'is_active': {'read_only': True},
+            'age': {'required': False}
+        }
+
+    def update(self, instance, validated_data):
+        # validated_data.pop('password') if validated_data.get('password') else None
+        if not self.context['request'].user.is_staff:
+            validated_data.pop('username') if validated_data.get('username') else None
+
+        return super().update(instance, validated_data)
+
+
+class CustomerCreateSerializer(DjoserUserCreateSerializer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["re_password"] = serializers.CharField(
+            style={"input_type": "password"},
+            write_only=True,
+        )
+
+    class Meta:
+        model = DjoserUserCreateSerializer.Meta.model
+        fields = [
+            'pk',
+            'username',
+            'password',
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+            'age',
+        ]
+        extra_kwargs = {
+            'email': {'write_only': True},
+        }
+
+
+class CustomerDeleteSerializer(serializers.Serializer):
+    pass
