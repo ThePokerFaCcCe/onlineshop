7692ca01a37106174fdf9ed758526c44e42c2404
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-09-11 20:16:08 +0430

    :sparkles: Add IsAdmin perm

--- user_perms/permissions.py
+++ user_perms/permissions.py
@@ -1,8 +1,23 @@
 from rest_framework import permissions
 
+
 class IsOwnerOfItem(permissions.BasePermission):
-    def has_object_permission(self,req,view,obj):
-        return obj.user == req.user
+    def has_object_permission(self, req, view, obj):
+        obj_user = None
+
+        if hasattr(obj, 'user'):
+            obj_user = obj.user
+        elif hasattr(obj, 'customer'):
+            obj_user = obj.customer
+        print(obj_user == req.user)
+        return obj_user == req.user
+
+class IsAdmin(permissions.IsAdminUser): 
+    def has_object_permission(self, request, view, obj):
+        # The reason I created this class and I didn't used IsAdminUser:
+        # In BasePermission class this function always returns True,
+        # so I have to override it to works correct with IsOwnerOfItem
+        return self.has_permission(request=request,view=view)
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, req, view):
@@ -10,14 +25,15 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             return True
         elif req.user.is_authenticated:
             return (req.user.is_staff or req.user.is_admin)
-    def has_object_permission(self, req, view,obj):
+
+    def has_object_permission(self, req, view, obj):
         if req.method in permissions.SAFE_METHODS:
             return True
         elif req.user.is_authenticated:
             return (req.user.is_staff or req.user.is_admin)
 
+
 class IsSuperUser(permissions.BasePermission):
     def has_permission(self, req, view):
         if req.user.is_authenticated:
             return req.user.is_admin
-        
