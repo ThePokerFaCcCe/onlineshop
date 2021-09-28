c11c9c980700555da02c874fc65ea5bdd410fd1f
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-09-28 04:58:37 +0330

    :hammer: Add example for field for docs.

--- picturic/serializer_fields.py
+++ picturic/serializer_fields.py
@@ -1,9 +1,23 @@
+from rest_framework import serializers
 from rest_framework.settings import api_settings
 from rest_framework.fields import ImageField
-from drf_spectacular.utils import extend_schema_field
+from drf_spectacular.utils import extend_schema_field, extend_schema_serializer, OpenApiExample
 from drf_spectacular.types import OpenApiTypes
 
-@extend_schema_field(OpenApiTypes.BINARY)
+
+@extend_schema_serializer(
+
+)
+class SchemaSerializer(serializers.Serializer):
+    pass
+
+
+@extend_schema_field({'type': "string", 'format': 'binary',
+                      'example': {
+                          "image": {"url": "string", "name": "string"},
+                          "thumbnail": {"url": "string", "name": "string"}
+                      }
+})
 class PictureField(ImageField):
     def _make_url(self, value):
         try:
@@ -16,14 +30,12 @@ class PictureField(ImageField):
         return url
 
     def _make_data(self, value):
-        if not value:
-            return None
         return {
-            'url': self._make_url(value),
-            'name': value.name,
+            'url': self._make_url(value) if value else '',
+            'name': value.name if value else '',
         }
 
-    def to_representation(self, value):
+    def to_representation(self, value) -> int:
         if not value:
             return None
         # use_url = getattr(self, 'use_url', api_settings.UPLOADED_FILES_USE_URL)
@@ -31,6 +43,7 @@ class PictureField(ImageField):
             'image': self._make_data(value.image),
             'thumbnail': self._make_data(value.thumbnail),
         }
+
     def to_internal_value(self, data):
         # print('data: ',data.__dict__) # For future usage
         return super().to_internal_value(data)
