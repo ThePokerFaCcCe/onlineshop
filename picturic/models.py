9359f276f25600c0a90ba1d42f831768166c88da
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-10-03 19:01:09 +0330

    :sparkles: Add generic picture model with PictureField

--- picturic/models.py
+++ picturic/models.py
@@ -1,5 +1,17 @@
-from .fields import PictureField
 from django.db.models import Model
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import related
+from django.db.models.fields.related import ForeignKey
+from django.db.models.fields import PositiveIntegerField
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-class PictureModel(Model):
-    pic=PictureField(make_thumbnail=True,use_upload_to_func=True)
+from .utils import upload_to_path_generic
+from .fields import PictureField
+
+class PictureGeneric(Model):
+    file = PictureField(make_thumbnail=True, upload_to=upload_to_path_generic)
+
+    content_type = ForeignKey(to=ContentType, on_delete=CASCADE,related_name="pictures")
+    object_id = PositiveIntegerField()
+    content_object = GenericForeignKey()
