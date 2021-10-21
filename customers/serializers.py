18700afa6933e7bbfe3c7ed6f765489c400118fb
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-10-21 19:55:58 +0330

    :hammer: Make customer serializers better

--- customers/serializers.py
+++ customers/serializers.py
@@ -21,13 +21,14 @@ class CustomerReadOnlySerializer(serializers.ModelSerializer):
 
 
 class CustomerSerializer(DjoserUserSerializer):
+    profile_image = PictureField(read_only=True)
+
     class Meta:
         model = DjoserUserSerializer.Meta.model
         fields = [
             'id',
             'username',
-            # 'email',
-            # 'password',
+            'email',
             'first_name',
             'last_name',
             'phone_number',
@@ -39,30 +40,23 @@ class CustomerSerializer(DjoserUserSerializer):
         ]
 
         extra_kwargs = {
-            # 'password': {'write_only': True},
-            # 'email': {'write_only': True},
             'is_active': {'read_only': True},
-            'profile_image': {'read_only': True},
+            'email': {'read_only': True},
+            'username': {'read_only': True},
             'age': {'required': False},
         }
 
-    def update(self, instance, validated_data):
-        # validated_data.pop('password') if validated_data.get('password') else None
-        if not self.context['request'].user.is_staff:
-            validated_data.pop('username') if validated_data.get('username') else None
-
-        return super().update(instance, validated_data)
 
 class CustomerProfileSerializer(serializers.ModelSerializer):
-    profile_image=PictureField(required=True)
+    profile_image = PictureField(required=True)
+
     class Meta:
-        model=Customer
-        fields=[
+        model = Customer
+        fields = [
             'pk',
             'profile_image',
         ]
-    
-    
+
 
 class CustomerCreateSerializer(DjoserUserCreateSerializer):
 
@@ -84,35 +78,36 @@ class CustomerCreateSerializer(DjoserUserCreateSerializer):
             'email',
             'phone_number',
             'age',
-            'profile_image',
         ]
         extra_kwargs = {
             'email': {'write_only': True},
-            'profile_image': {'read_only': True},
         }
 
 
 class CustomerDeleteSerializer(serializers.Serializer):
     pass
 
+
 class CustomerToAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields=[
+        fields = [
             'id',
             'is_staff',
             'is_admin',
         ]
-        extra_kwargs={
-            'is_staff':{'source':'staff','required':True},
-            'is_admin':{'source':'admin','required':True},
+        extra_kwargs = {
+            'is_staff': {'source': 'staff', 'required': True},
+            'is_admin': {'source': 'admin', 'required': True},
         }
+
 
 class AddressSerializer(serializers.ModelSerializer):
     country = CountryField(country_dict=True)
+
     class Meta:
-        model=Address
-        fields=[
+        model = Address
+        fields = [
             'id',
             'country',
             'city',
@@ -120,6 +115,6 @@ class AddressSerializer(serializers.ModelSerializer):
             'postal_code',
             'user',
         ]
-        extra_kwargs={
-            'user':{'source':'customer','read_only':True},
+        extra_kwargs = {
+            'user': {'source': 'customer', 'read_only': True},
         }
