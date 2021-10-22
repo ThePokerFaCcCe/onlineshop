d36654e869696b6437e3995342200e54711ac61f
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2021-10-22 17:36:58 +0330

    :hammer: Recreate Comment model and use MPTTModel

--- social_media/models.py
+++ social_media/models.py
@@ -2,11 +2,10 @@ from django.db.models.base import Model
 from django.db.models.deletion import CASCADE, PROTECT
 from django.db.models.fields import CharField, DateTimeField, PositiveIntegerField, TextField, BooleanField
 from django.db.models.fields.related import ForeignKey
-
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-
 from django.conf import settings
+from mptt.models import MPTTModel, TreeForeignKey
 
 User = settings.AUTH_USER_MODEL
 
@@ -34,15 +33,19 @@ class Like(Model):
     content_object = GenericForeignKey()
 
 
-class Comment(Model):
+class Comment(MPTTModel):
     text = TextField(max_length=300)
     hidden = BooleanField(default=False,blank=True)
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
     
     user = ForeignKey(to=User,on_delete=CASCADE)
-    reply_to = ForeignKey(to='Comment', on_delete=CASCADE, related_name="reply",null=True,blank=True)
+    reply_to = TreeForeignKey(to='self', on_delete=CASCADE, related_name="reply",null=True,blank=True)
 
     content_type = ForeignKey(to=ContentType,on_delete=CASCADE)
     object_id = PositiveIntegerField()
     content_object = GenericForeignKey()
+
+    class MPTTMeta:
+        order_insertion_by = ['created_at']
+        parent_attr = 'reply_to'
