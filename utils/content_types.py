0296b6221435a968954c9e92308261f5e5e1732f
thepokerfaccce <thepokerfa3e@gmail.com>
2021-11-20 19:35:53 +0330

    :bug: Fix ProgrammingError for first migration

--- utils/content_types.py
+++ utils/content_types.py
@@ -3,9 +3,10 @@ from django.db.utils import ProgrammingError
 
 from products.models import Product
 
+PRODUCT = None
 
 try:
-    ContentType.objects.none()
+    ContentType.objects.get_for_model(Product)
 
 except ProgrammingError:
     print("WARNING: We couldn't get ContentType model in database, if you are trying to migrate, don't care about this message")
